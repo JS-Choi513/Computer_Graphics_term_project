cb26549b928a58397d6d7e2dd03d2cf9badea5c7
hwang mandy <2020304@cwnu.ac.kr>
2021-11-24 18:43:34 +0900

    add feature 2

--- src/Baseline.cpp
+++ src/Baseline.cpp
@@ -1,4 +1,4 @@
-﻿/* This program illustrates the use of the glut library for
+/* This program illustrates the use of the glut library for
 interfacing with a window system */
 
 //사용자 정의 상수
@@ -8,13 +8,21 @@ interfacing with a window system */
 #define TRIANGLE  3
 #define POINTS 4
 #define TEXT 5
+#define CIRCLE 6
+#define ERASER 7
+#define SPRAY 8
+#define DRAW 9 // 브러쉬
 
 #include <glut.h>
 #include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
 
 void mouse(int, int, int, int);
+void mouse2(int, int);
 void key(unsigned char, int, int);
 void display(void);
+void display2(void); //메뉴창만 초기화
 void drawSquare(int, int);
 void myReshape(GLsizei, GLsizei);
 
@@ -28,21 +36,30 @@ void pixel_menu(int);
 void fill_menu(int);
 int pick(int, int);
 
+void line_menu(int); //라인 모양 메뉴
+void random_color(); //랜덤 색상
+void brush(int, int); //브러쉬
+void bcolor_menu(int); //배경 색상
+void line_dotted(int); //점선
+
 /* globals */
 
 GLsizei wh = 500, ww = 500; /* initial window size */
 GLfloat size = 3.0;   /* half side length of square */
 int draw_mode = 0; /* drawing mode */
 int rx, ry; /*raster position*/
+int cursorX, cursorY;
+int line = 0; //라인 메뉴 값
+int ran_check = 0; //랜덤 색상 적용 유무
 
 GLfloat r = 1.0, g = 1.0, b = 1.0; /* drawing color */
+GLfloat r2 = 0.8, g2 = 0.8, b2 = 0.8; /* 배경 색상 */
 int fill = 0; /* fill flag */
 
 //사각형 모양의 점 출력
 void drawSquare(int x, int y)
 {
     y = wh - y;
-    //랜덤 색
     glColor3ub((char)rand() % 256, (char)rand() % 256, (char)rand() % 256);
     glBegin(GL_POLYGON);
     glVertex2f(x + size, y + size);
@@ -55,7 +72,7 @@ void drawSquare(int x, int y)
 
 /* rehaping routine called whenever window is resized
 or moved */
-//창조절 크기
+
 void myReshape(GLsizei w, GLsizei h)
 {
 
@@ -73,7 +90,7 @@ void myReshape(GLsizei w, GLsizei h)
     glClearColor(0.8, 0.8, 0.8, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     display();
-    glFlush();
+    //glFlush();
 
     /* set global size for use by drawing routine */
 
@@ -114,109 +131,221 @@ void mouse(int btn, int state, int x, int y)
     {
         glPushAttrib(GL_ALL_ATTRIB_BITS); //?
 
+        if (ran_check == 1)	// 1이면 랜덤 색상 적용
+            random_color();
+
         //마우스 왼쪽 버튼이 눌린  좌표 저장 
         where = pick(x, y);
+        cursorX = x;
+        cursorY = y;
+        line_dotted(line); //점선 속성 설정
         glColor3f(r, g, b);
+        glLineWidth(size); //두께 설정
+
         //눌린 위치가 메뉴 창인가 아닌가 판단
         if (where != 0)
         {
             count = 0;
             draw_mode = where;
+            cursorX = 0;
+            cursorY = 0;
         }
         //실제 동작 들어감
         else switch (draw_mode)
-        {   
+        {
             //case 1. Line인 경우
-            case(LINE):
-                //처음 마우스가 눌린 경우(첫좌표 저장)
-                if (count == 0)
-                {
-                    count++; //그래서 눌렸으니 ++
-                    xp[0] = x;
-                    yp[0] = y;
-                }
-                //두번째 눌렷을 경우
-                else
-                {
-                    glBegin(GL_LINES);
-                    //받아온 좌표와
-                    glVertex2i(x, wh - y);
-                    //저장된 자표를 이음
-                    glVertex2i(xp[0], wh - yp[0]);
-                    glEnd();
-                    draw_mode = 0;
-                    count = 0;
-                }
-                break;
-            //사각형
-            case(RECTANGLE):
-                //첫좌표 저장
-                if (count == 0)
-                {
-                    count++;
-                    xp[0] = x;
-                    yp[0] = y;
-                }
-                else
-                {
-                    //채움 사각형 그리는 케이스
-                    if (fill) glBegin(GL_POLYGON);
-                    //아니라면 선 4개를 그려서 그냥 사각형 그리기
-                    else glBegin(GL_LINE_LOOP);
-                    glVertex2i(x, wh - y);
-                    glVertex2i(x, wh - yp[0]);
-                    glVertex2i(xp[0], wh - yp[0]);
-                    glVertex2i(xp[0], wh - y);
-                    glEnd();
-                    draw_mode = 0;
-                    count = 0;
-                }
-                break;
-            //사각형과 같음 대신 점 세개가 필요하니 count 1 2 추가
-            case (TRIANGLE):
-                switch (count)
-                {
-                case(0):
-                    count++;
-                    xp[0] = x;
-                    yp[0] = y;
-                    break;
-                case(1):
-                    count++;
-                    xp[1] = x;
-                    yp[1] = y;
-                    break;
-                case(2):
-                    if (fill) glBegin(GL_POLYGON);
-                    else glBegin(GL_LINE_LOOP);
-                    glVertex2i(xp[0], wh - yp[0]);
-                    glVertex2i(xp[1], wh - yp[1]);
-                    glVertex2i(x, wh - y);
-                    glEnd();
-                    draw_mode = 0;
-                    count = 0;
-                }
-                break;
-            //위의 네모점 찍기 추가
-            case(POINTS):
+        case(LINE):
+            //처음 마우스가 눌린 경우(첫좌표 저장)
+            if (count == 0)
             {
-                drawSquare(x, y);
-                count++;
+                count++; //그래서 눌렸으니 ++
+                xp[0] = x;
+                yp[0] = y;
             }
-            break;
-            case(TEXT):
+            //두번째 눌렷을 경우
+            else
             {
-                rx = x;
-                ry = wh - y;
-                glRasterPos2i(rx, ry);
+                glBegin(GL_LINES);
+                //받아온 좌표와
+                glVertex2i(x, wh - y);
+                //저장된 자표를 이음
+                glVertex2i(xp[0], wh - yp[0]);
+                glEnd();
+                draw_mode = 0;
                 count = 0;
             }
+            break;
+            //사각형
+        case(RECTANGLE):
+            //첫좌표 저장
+            if (count == 0)
+            {
+                count++;
+                xp[0] = x;
+                yp[0] = y;
+            }
+            else
+            {
+                //채움 사각형 그리는 케이스
+                if (fill) glBegin(GL_POLYGON);
+                //아니라면 선 4개를 그려서 그냥 사각형 그리기
+                else glBegin(GL_LINE_LOOP);
+                glVertex2i(x, wh - y);
+                glVertex2i(x, wh - yp[0]);
+                glVertex2i(xp[0], wh - yp[0]);
+                glVertex2i(xp[0], wh - y);
+                glEnd();
+                draw_mode = 0;
+                count = 0;
+            }
+            break;
+            //사각형과 같음 대신 점 세개가 필요하니 count 1 2 추가
+        case (TRIANGLE):
+            switch (count)
+            {
+            case(0):
+                count++;
+                xp[0] = x;
+                yp[0] = y;
+                break;
+            case(1):
+                count++;
+                xp[1] = x;
+                yp[1] = y;
+                break;
+            case(2):
+                if (fill) glBegin(GL_POLYGON);
+                else glBegin(GL_LINE_LOOP);
+                glVertex2i(xp[0], wh - yp[0]);
+                glVertex2i(xp[1], wh - yp[1]);
+                glVertex2i(x, wh - y);
+                glEnd();
+                draw_mode = 0;
+                count = 0;
+            }
+            break;
+            //위의 사각형 점 찍기 추가
+        case(POINTS):
+        {
+            drawSquare(x, y);
+            count++;
         }
+        break;
+        case(TEXT):
+        {
+            rx = x;
+            ry = wh - y;
+            glRasterPos2i(rx, ry);
+            count = 0;
+        }
+        //원그리기
+        case(CIRCLE):
+        {
+            if (count == 0) {
+                count++;
+                xp[0] = x;
+                yp[0] = y;
+                break;
+            }
+            else {
+                double cx, cy, angle, i;
+                //반지름 구하기
+                double r = fabs(sqrt((xp[0] - x) * (xp[0] - x) + (yp[0] - y) * (yp[0] - y)));
+                if (fill) glBegin(GL_POLYGON);
+                else glBegin(GL_POINTS);
+                for (i = 0.0; i <= 3600; i += 0.036) {
+                    angle = i * 3.141592 / 180.0;
+                    glVertex2f((r * cos(angle)) + xp[0], (r * sin(angle)) + wh - yp[0]);
+                }
+                glEnd();
+            }
+            display2();
+            draw_mode = 0;
+            count = 0;
+            break;
+        }
+        case(ERASER):
+            glColor3f(0.8, 0.8, 0.8);
+            y = wh - y;
+            double i, angle;
+            if (x < ww) {
+                glBegin(GL_POLYGON);
+                for (i = 0.0; i <= 3600; i += 0.036) {
+                    angle = i * 3.141592 / 180.0;
+                    glVertex2f((10 * cos(angle)) + x, (10 * sin(angle)) + y);
+                }
+            }
+            glEnd();
 
+            break;
+
+        }
         glPopAttrib();
         glFlush();
+
     }
 }
+
+//지우개
+void mouse2(int x, int y) {
+    int n_y = wh - y;
+    double i, angle;
+
+    switch (draw_mode) {
+    //지우개 모드일때
+    case(ERASER):
+        //현재 마우스 위치에 원하나 그리고    
+        glColor3f(0.8, 0.8, 0.8);
+
+        glBegin(GL_POLYGON);
+        for (i = 0.0; i <= 3600; i += 0.036) {
+            angle = i * 3.141592 / 180.0;
+            glVertex2f((10 * cos(angle)) + x, (10 * sin(angle)) + n_y);
+        }
+        glEnd();
+        display2();
+        // 마우스 좌표 갱신
+        cursorX = x;
+        cursorY = y;
+        break;
+
+    case(SPRAY):
+        glColor3f(r, g, b);
+        glBegin(GL_POINTS);
+        for (i = 0.0; i <= 360; i += 36) {
+            angle = i * 3.141592 / 180.0;
+            //mod 4를 통해 지정된 범위에서 점 찍기
+            glVertex2f(rand() % 4 * (10 * cos(angle)) + x, rand() % 4 * (10 * sin(angle)) + n_y);
+        }
+        glEnd();
+        display2(); //도형 선택창 침범 x
+        // 마우스 좌표 갱신
+        glPointSize(1.0);
+        cursorX = x;
+        cursorY = y;
+        break;
+
+    case(DRAW):
+        glColor3f(r, g, b);
+
+        glBegin(GL_POLYGON);
+        for (i = 0.0; i <= 3600; i += 0.036) {
+            angle = i * 3.141592 / 180.0;
+            glVertex2f((10 * cos(angle)) + x, (10 * sin(angle)) + n_y);
+        }
+        glEnd();
+        display2();
+        cursorX = x;
+        cursorY = y;
+
+        break;
+
+    }
+
+    glPopAttrib();
+    glFlush();
+}
+
 
 //마우스가 특정 지점에서 클릭되면 해당 지점이 지시하는 값 선택
 int pick(int x, int y)
@@ -224,14 +353,19 @@ int pick(int x, int y)
     y = wh - y;
     if (y < wh - ww / 10) return 0;
     else if (x < ww / 10) return LINE;
-    else if (x < ww / 5) return RECTANGLE;
+    else if (x < 2 * ww / 10) return RECTANGLE;
     else if (x < 3 * ww / 10) return TRIANGLE;
-    else if (x < 2 * ww / 5) return POINTS;
-    else if (x < ww / 2) return TEXT;
+    else if (x < 4 * ww / 10) return POINTS;
+    else if (x < 5 * ww / 10)  return TEXT;
+
+    else if (x < 6 * ww / 10) return CIRCLE;
+    else if (x < 7 * ww / 10) return ERASER;
+    else if (x < 8 * ww / 10) return SPRAY;
+    else if (x < 9 * ww / 10) return DRAW;
     else return 0;
 }
 
-//아마 사각형을 그리는 함수인듯
+//아마 사각형을 그리는 함수인듯???
 void screen_box(int x, int y, int s)
 {
     glBegin(GL_QUADS);
@@ -255,7 +389,7 @@ void middle_menu(int id)
 
 }
 
-//색을 지정하는 메뉴
+//도형 및 글자들의 색을 지정하는 메뉴
 void color_menu(int id)
 {
     if (id == 1) { r = 1.0; g = 0.0; b = 0.0; }
@@ -266,6 +400,7 @@ void color_menu(int id)
     else if (id == 6) { r = 1.0; g = 1.0; b = 0.0; }
     else if (id == 7) { r = 1.0; g = 1.0; b = 1.0; }
     else if (id == 8) { r = 0.0; g = 0.0; b = 0.0; }
+
 }
 
 //글자크기를 조절하는 동작
@@ -293,30 +428,112 @@ void key(unsigned char k, int xx, int yy)
     rx += glutBitmapWidth(GLUT_BITMAP_9_BY_15, k);
 
 }
+/*----------------------------------------------------------------------*/
+void random_color() //랜덤 컬러 생성
+{
+    r = (float)(rand() % 256) / 255;
+    g = (float)(rand() % 256) / 255;
+    b = (float)(rand() % 256) / 255;
+}
+
+void bcolor_menu(int id) { //배경색 지정 및 랜덤 설정
+    if (id == 1) { r2 = 1.0; g2 = 0.0; b2 = 0.0; }
+    else if (id == 2) { r2 = 0.0; g2 = 1.0; b2 = 0.0; }
+    else if (id == 3) { r2 = 0.0; g2 = 0.0; b2 = 1.0; }
+    else if (id == 4) { r2 = 0.0; g2 = 1.0; b2 = 1.0; }
+    else if (id == 5) { r2 = 1.0; g2 = 0.0; b2 = 1.0; }
+    else if (id == 6) { r2 = 1.0; g2 = 1.0; b2 = 0.0; }
+    else if (id == 7) { r2 = 1.0; g2 = 1.0; b2 = 1.0; }
+    else if (id == 8) { r2 = 0.0; g2 = 0.0; b2 = 0.0; }
+    else if (id == 9) { r2 = 0.8; g2 = 0.8; b2 = 0.8; }
+    else {
+        r2 = (float)(rand() % 256) / 255;
+        g2 = (float)(rand() % 256) / 255;
+        b2 = (float)(rand() % 256) / 255;
+    }
+    display();
+}
+
+
+void line_dotted(int id) //점선
+{
+    if (id == 1) {
+        glEnable(GL_LINE_STIPPLE);
+        glLineStipple(1, 0xAAAA);
+    }
+    else if (id == 2) {
+        glEnable(GL_LINE_STIPPLE);
+        glLineStipple(2, 0xAAAA);
+    }
+    else if (id == 3) {
+        glEnable(GL_LINE_STIPPLE);
+        glLineStipple(3, 0xAAAA);
+    }
+    else if (id == 4) {
+        glEnable(GL_LINE_STIPPLE);
+        glLineStipple(4, 0xAAAA);
+    }
+    else if (id == 5) {
+        glEnable(GL_LINE_STIPPLE);
+        glLineStipple(1, 0x00FF);
+    }
+    else if (id == 6) {
+        glEnable(GL_LINE_STIPPLE);
+        glLineStipple(2, 0x00FF);
+    }
+    else
+        glDisable(GL_LINE_STIPPLE);
+}
+
+void line_menu(int id)
+{
+    line = id;
+}
+
+
+/*----------------------------------------------------------------------*/
+
+
 
 //윈도우에 메뉴 창 그리기
 void display(void)
 {
     int shift = 0;
     glPushAttrib(GL_ALL_ATTRIB_BITS);
-    glClearColor(0.8, 0.8, 0.8, 1.0);
+    glClearColor(r2, g2, b2, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 1.0, 1.0);
     screen_box(0, wh - ww / 10, ww / 10);
     glColor3f(1.0, 0.0, 0.0);
     screen_box(ww / 10, wh - ww / 10, ww / 10);
     glColor3f(0.0, 1.0, 0.0);
-    screen_box(ww / 5, wh - ww / 10, ww / 10);
+    screen_box(2* ww / 10, wh - ww / 10, ww / 10);
     glColor3f(0.0, 0.0, 1.0);
     screen_box(3 * ww / 10, wh - ww / 10, ww / 10);
     glColor3f(1.0, 1.0, 0.0);
-    screen_box(2 * ww / 5, wh - ww / 10, ww / 10);
+    screen_box(4 * ww / 10, wh - ww / 10, ww / 10);
+    /*-------------------------------------------------------*/
+    //ui 구현 - 메뉴 사각형 추가
+    glColor3f(1.0, 1.0, 1.0);
+    screen_box(5 * ww / 10, wh - ww / 10, ww / 10);
+    glColor3f(0.0, 1.0, 1.0);
+    screen_box(6*  ww / 10, wh - ww / 10, ww / 10);
+    glColor3f(1.0, 0.0, 1.0);
+    screen_box(7* ww / 10, wh - ww / 10, ww / 10);
+    
+    glColor3f(0.5, 0.1, 0.1); //브러쉬 메뉴 박스
+    screen_box(8 * ww / 10, wh - ww / 10, ww / 10);
+    /*-------------------------------------------------------*/
+
+    //ui 사각형
     glColor3f(0.0, 0.0, 0.0);
     screen_box(ww / 10 + ww / 40, wh - ww / 10 + ww / 40, ww / 20);
+    //ui 선
     glBegin(GL_LINES);
     glVertex2i(wh / 40, wh - ww / 20);
     glVertex2i(wh / 40 + ww / 20, wh - ww / 20);
     glEnd();
+    //ui 삼각형
     glBegin(GL_TRIANGLES);
     glVertex2i(ww / 5 + ww / 40, wh - ww / 10 + ww / 40);
     glVertex2i(ww / 5 + ww / 20, wh - ww / 40);
@@ -334,14 +551,78 @@ void display(void)
     shift += glutBitmapWidth(GLUT_BITMAP_9_BY_15, 'B');
     glRasterPos2i(2 * ww / 5 + shift, wh - ww / 20);
     glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'C');
+    glEnd();
+    /*-------------------------------------------------------*/
+    //ui 구현 - 메뉴 사각형 추가
+
+    /*-------------------------------------------------------*/
     glFlush();
     glPopAttrib();
 }
 
+//메뉴창만 초기화
+void display2(void) {
+    int shift = 0;
+    glColor3f(1.0, 1.0, 1.0);
+    screen_box(0, wh - ww / 10, ww / 10);
+    glColor3f(1.0, 0.0, 0.0);
+    screen_box(ww / 10, wh - ww / 10, ww / 10);
+    glColor3f(0.0, 1.0, 0.0);
+    screen_box(ww / 5, wh - ww / 10, ww / 10);
+    glColor3f(0.0, 0.0, 1.0);
+    screen_box(3 * ww / 10, wh - ww / 10, ww / 10);
+    glColor3f(1.0, 1.0, 0.0);
+    screen_box(2 * ww / 5, wh - ww / 10, ww / 10);
+    /*-------------------------------------------------------*/
+    //ui 구현 - 메뉴 사각형 추가
+
+    glColor3f(1.0, 1.0, 1.0);
+    screen_box(5 * ww / 10, wh - ww / 10, ww / 10);
+    glColor3f(0.0, 1.0, 1.0);
+    screen_box(6 * ww / 10, wh - ww / 10, ww / 10);
+    glColor3f(1.0, 0.0, 1.0);
+    screen_box(7 * ww / 10, wh - ww / 10, ww / 10);
+
+    glColor3f(0.5, 0.1, 0.1);
+    screen_box(8 * ww / 10, wh - ww / 10, ww / 10);
+    /*-------------------------------------------------------*/
+    //ui 사각형
+    glColor3f(0.0, 0.0, 0.0);
+    screen_box(ww / 10 + ww / 40, wh - ww / 10 + ww / 40, ww / 20);
+    //ui 선
+    glBegin(GL_LINES);
+    glVertex2i(wh / 40, wh - ww / 20);
+    glVertex2i(wh / 40 + ww / 20, wh - ww / 20);
+    glEnd();
+    //ui 삼각형
+    glBegin(GL_TRIANGLES);
+    glVertex2i(ww / 5 + ww / 40, wh - ww / 10 + ww / 40);
+    glVertex2i(ww / 5 + ww / 20, wh - ww / 40);
+    glVertex2i(ww / 5 + 3 * ww / 40, wh - ww / 10 + ww / 40);
+    glEnd();
+    glPointSize(3.0);
+    glBegin(GL_POINTS);
+    glVertex2i(3 * ww / 10 + ww / 20, wh - ww / 20);
+    glEnd();
+    glRasterPos2i(2 * ww / 5, wh - ww / 20);
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'A');
+    shift = glutBitmapWidth(GLUT_BITMAP_9_BY_15, 'A');
+    glRasterPos2i(2 * ww / 5 + shift, wh - ww / 20);
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'B');
+    shift += glutBitmapWidth(GLUT_BITMAP_9_BY_15, 'B');
+    glRasterPos2i(2 * ww / 5 + shift, wh - ww / 20);
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'C');
+    /*-------------------------------------------------------*/
+    //ui 구현 - 메뉴 사각형 추가
+
+    /*-------------------------------------------------------*/
+    glFlush();
+    glPopAttrib();
+}
 
 int main(int argc, char** argv)
 {
-    int c_menu, p_menu, f_menu;
+    int c_menu, p_menu, f_menu, b_menu, l_menu;
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -361,15 +642,36 @@ int main(int argc, char** argv)
     glutAddMenuEntry("White", 7);
     glutAddMenuEntry("Black", 8);
 
-    //color_menu의 형태와 같음222
+    //color_menu의 형태와 같음
     p_menu = glutCreateMenu(pixel_menu);
     glutAddMenuEntry("increase pixel size", 1);
     glutAddMenuEntry("decrease pixel size", 2);
 
-    //333
+    //이하 동문
     f_menu = glutCreateMenu(fill_menu);
     glutAddMenuEntry("fill on", 1);
     glutAddMenuEntry("fill off", 2);
+
+    b_menu = glutCreateMenu(bcolor_menu);
+    glutAddMenuEntry("Red", 1);
+    glutAddMenuEntry("Green", 2);
+    glutAddMenuEntry("Blue", 3);
+    glutAddMenuEntry("Cyan", 4);
+    glutAddMenuEntry("Magenta", 5);
+    glutAddMenuEntry("Yellow", 6);
+    glutAddMenuEntry("White", 7);
+    glutAddMenuEntry("Black", 8);
+    glutAddMenuEntry("Default", 9);
+    glutAddMenuEntry("Random Color", 10);
+
+    l_menu = glutCreateMenu(line_menu);
+    glutAddMenuEntry("Dotted Line 1", 1);
+    glutAddMenuEntry("Dotted Line 2", 2);
+    glutAddMenuEntry("Dotted Line 3", 3);
+    glutAddMenuEntry("Dotted Line 4", 4);
+    glutAddMenuEntry("Dotted Line 5", 5);
+    glutAddMenuEntry("Dotted Line 6", 6);
+    glutAddMenuEntry("Full Line", 7);
 
     //오른쪽 마우스 버튼 눌릴시 나타나는 메뉴 창
     glutCreateMenu(right_menu);
@@ -384,12 +686,16 @@ int main(int argc, char** argv)
     glutAddSubMenu("Colors", c_menu);
     glutAddSubMenu("Pixel Size", p_menu);
     glutAddSubMenu("Fill", f_menu);
+    glutAddSubMenu("Background Color", b_menu);
+    glutAddSubMenu("Line", l_menu);
     glutAttachMenu(GLUT_MIDDLE_BUTTON);
 
     myinit();
     glutReshapeFunc(myReshape);
     glutKeyboardFunc(key);
     glutMouseFunc(mouse);
+    //마우스 모션 함수
+    glutMotionFunc(mouse2);
     glutMainLoop();
 
 }
